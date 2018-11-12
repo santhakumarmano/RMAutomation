bc6f4c034aeb957043cc6b6ec8f1213386be282f
padmanabhanvj9 <padmanabhanvj9@gmail.com>
2018-11-12 20:52:31 +0530

    Login-->done,DashBoard-->onGoing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  {path: '',   redirectTo: '/login', pathMatch: 'full'},
+  { path: 'dashboard', component:DashboardComponent},
+  {path: '',   redirectTo: '/dashboard', pathMatch: 'full'},
 ];
 
 @NgModule({
